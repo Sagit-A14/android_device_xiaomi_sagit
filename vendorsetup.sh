0e37793761a714c9fd5d51a29f6ba7bb115e7fa7
denysaja <143594138+denysaja@users.noreply.github.com>
2024-08-11 11:15:32 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-git clone --recurse-submodules -b lineage-21 https://github.com/Sagit-A14/android_kernel_xiaomi_msm8998.git kernel/xiaomi/msm8998
+git clone -b lineage-21 https://github.com/Sagit-A14/android_kernel_xiaomi_msm8998.git kernel/xiaomi/msm8998

1f4a590518722c6e4dcbdb0bdc59e491fa79fb71
deny <77781846+deny2311@users.noreply.github.com>
2024-08-14 09:12:13 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,1 @@
-git clone -b lineage-21-su https://github.com/Sagit-A14/android_kernel_xiaomi_msm8998.git kernel/xiaomi/msm8998
-cd kernel/xiaomi/msm8998 && rm -rf KernelSU && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5 && cd ../../../
+git clone -b lineage-21 https://github.com/Sagit-A14/android_kernel_xiaomi_msm8998.git kernel/xiaomi/msm8998

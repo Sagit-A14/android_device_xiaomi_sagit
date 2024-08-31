3fd0ce5c014f1c775412d273b7f4dcc4e32f8fd5
deny <77781846+deny2311@users.noreply.github.com>
2024-08-31 11:30:11 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
+rm -rf kernel/xiaomi/msm8998
 git clone -b lineage-21 https://github.com/Sagit-A14/android_kernel_xiaomi_msm8998.git kernel/xiaomi/msm8998
-git clone -b fourteen https://gitlab.com/yaosp/prebuilts_clang_host_linux-x86.git prebuilts/clang/host/linux-x86
+rm -rf prebuilts/clang/host/linux-x86/clang-r487747c
+git clone -b master https://www.opencode.net/clhexftw/android_prebuilts_clang_kernel_linux-x86_clang-clang-r487747c.git prebuilts/clang/host/linux-x86/clang-r487747c

4edc9b0f503c51d0078547fa98af0dabb8661f46
deny <77781846+deny2311@users.noreply.github.com>
2024-09-10 12:01:22 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
 rm -rf prebuilts/clang/host/linux-x86/clang-r510928
 git clone -b main https://gitlab.com/dogpoopy/clang-r510928.git prebuilts/clang/host/linux-x86/clang-r510928
+rm -rf frameworks/native
+git clone -b fourteen https://github.com/RisingTechOSS/android_frameworks_native.git frameworks/native

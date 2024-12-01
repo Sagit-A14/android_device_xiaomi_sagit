5128424757b823141945e8c12b7b8572877487e8
deny <77781846+deny2311@users.noreply.github.com>
2024-12-01 16:45:31 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
-rm -rf prebuilts/clang/host/linux-x86/clang-r510928
-git clone -b main https://gitlab.com/dogpoopy/clang-r510928.git prebuilts/clang/host/linux-x86/clang-r510928
 rm -rf frameworks/native
 git clone -b alpha-14 https://github.com/Sagit-A14/frameworks_native_alpha14.git frameworks/native
+rm -rf prebuilts/clang/host/linux-x86/clang-r510928
+git clone -b main https://gitlab.com/clangsantoni/clang-r510928.git prebuilts/clang/host/linux-x86/clang-r510928
+rm -rf hardware/xiaomi
+git clone -b lineage-21 https://github.com/Sagit-A15/android_hardware_xiaomi.git hardware/xiaomi

5729403e98b98742f6bebace5b4e4955ff3354a7
deny <77781846+deny2311@users.noreply.github.com>
2024-12-05 09:47:34 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,6 @@ rm -rf prebuilts/clang/host/linux-x86/clang-r510928
 git clone -b main https://gitlab.com/clangsantoni/clang-r510928.git prebuilts/clang/host/linux-x86/clang-r510928
 rm -rf hardware/xiaomi
 git clone -b lineage-21 https://github.com/Sagit-A15/android_hardware_xiaomi.git hardware/xiaomi
+rm -rf kernel/xiaomi/msm8998
+git clone --recurse-submodules -b lineage-21 https://github.com/Sagit-A14/android_kernel_xiaomi_msm8998.git kernel/xiaomi/msm8998
+rm -rf system/extras/su
